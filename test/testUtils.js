801b605b2dfe29e9ab22dc3edb17447ecbd02ca1
Nebojsa Konstantinovic <nebojsa.konstantin@gmail.com>
2019-02-09 20:50:33 +0100

    jotto part 1

---
+++ test/testUtils.js
@@ -0,0 +1,10 @@
+/**
+ * Return node(s) with the given data-test attribute.
+ * @param {shallowWrapper} wrapper - Enzyme shallow wrapper.
+ * @param {string} val - Value of data-test attribute for search.
+ * @returns {shallowWrapper}
+ */
+
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`);
+}
