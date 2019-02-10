4825d664c417e26a719131d9788884e3f68d806c
Nebojsa Konstantinovic <nebojsa.konstantin@gmail.com>
2019-02-10 13:53:32 +0100

    Bootstrap styling added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import GuessedWords from './GuessedWords';
+import Congrats from './Congrats';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        patka
+      <div className="container">
+        <h1>Jottto</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[
+          { guessedWord: 'train', letterMatchCount: 3, },
+        ]} />
       </div>
     );
   }
